ddb8a872190d2d4a97d929569ec1c9890d475cb0
unknown <azizktata>
2026-01-03 09:30:01 +0100

    small ui adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,24 @@ import { ArrowUpRight, Github, Linkedin } from "lucide-react";
 import thelandlord from "../public/thelandlord.png";
 import CardProject from "@/components/card-project";
 import Link from "next/link";
+import { Badge } from "@/components/ui/badge";
+import CardExperiance from "@/components/card-experiance";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background ">
       {/* lg:py-32 lg:px-16 */}
       <main className="flex min-h-screen w-full max-w-6xl flex-col justify-between px-8 md:px-6 items-start md:flex-row md:gap-16 ">
-        <div id="left" className="flex flex-col pt-16 lg:mb-0 lg:sticky lg:top-0 md:py-32 md:w-1/2 mb-16 ">
-          <h1 className="text-5xl md:text-4xl lg:text-5xl font-bold tracking-wider mb-1">Aziz Ktata</h1>
+        <div
+          id="left"
+          className="flex flex-col pt-16 lg:mb-0 lg:sticky lg:top-0 md:py-32 md:w-1/2 mb-16 "
+        >
+          <h1 className="text-5xl md:text-4xl lg:text-5xl font-bold tracking-wider mb-1">
+            Aziz Ktata
+          </h1>
           <h3 className="text-lg mb-4">Full stack developer</h3>
           <p className="text-sm max-w-lg text-foreground mb-12">
-            I design, build web applications for businesses 
+            I design, build web applications for businesses
           </p>
           <div className="flex gap-4">
             <Button
@@ -37,9 +44,31 @@ export default function Home() {
             text ever since the 1500s, when an unknown printer took a galley of
             type and scrambled it to make a type specimen book.
           </p>
-
+          <div id="experiance" className="mb-12">
+            <h2 className="uppercase font-bold text-md mb-6">Experiance</h2>
+            <div className="flex flex-col gap-6 items-stretch">
+              <CardExperiance
+                timeline="2026 - Present"
+                position="Full Stack Developer"
+                description="Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy text
+                ever since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book."
+                techStack={["Laravel", "React", "Tailwind CSS", "MySQL"]}
+              />
+              <CardExperiance
+                timeline="2024 - 2026"
+                position="Frontend Developer"
+                description="Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy text
+                ever since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book."
+                techStack={["Next.js", "TypeScript", "Tailwind CSS", "Prisma"]}
+              />
+            </div>
+          </div>
           <div id="projects" className="">
-            <h2 className="text-md font-bold mb-4 uppercase">Projects</h2>
+            <h2 className="text-md font-bold mb-6 uppercase">Projects</h2>
 
             <CardProject
               title="Built property rental management system"
@@ -71,12 +100,9 @@ export default function Home() {
               <Button
                 variant="outline"
                 size="lg"
-                className="w-fit text-primary border-primary hover:bg-primary/10 my-4 "
+                className="w-fit text-primary border-primary hover:bg-primary/10 mb-4 "
               >
-                View all projects   <ArrowUpRight
-              size={16}
-              className=" ml-2"
-            />
+                View all projects <ArrowUpRight size={16} className=" ml-2" />
               </Button>
             </Link>
           </div>
